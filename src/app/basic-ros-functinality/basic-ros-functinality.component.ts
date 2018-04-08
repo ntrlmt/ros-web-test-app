30692288d0ba2eca1ebfb19c275d0cca68a05fe3
ntrlmt <m.moto.factory@gmail.com>
2018-04-08 13:00:08 +0900

    Modified

--- src/app/basic-ros-functinality/basic-ros-functinality.component.ts
+++ src/app/basic-ros-functinality/basic-ros-functinality.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Ros, Topic, Message } from 'roslib';
+import { Ros, Topic, Message, Service, ServiceRequest, ServiceResponse, Param } from 'roslib';
 
 @Component({
   selector: 'app-basic-ros-functinality',
@@ -8,10 +8,12 @@ import { Ros, Topic, Message } from 'roslib';
   styleUrls: ['./basic-ros-functinality.component.css']
 })
 export class BasicRosFunctinalityComponent implements OnInit {
-  private ros: Ros = undefined;
-  private listener: Topic = undefined;
-  subscribeMessage: string;
+  private ros: Ros;
   connected: boolean = false;
+  private listener: Topic;
+  subscribeMessage: string;
+  private addTwoIntsClient: Service;
+  num: number;
   constructor() { }
 
   ngOnInit() {
@@ -38,6 +40,14 @@ export class BasicRosFunctinalityComponent implements OnInit {
     this.listener.unsubscribe();
   }
 
+  private serviceEvent = (result) => {
+    console.log('Result for service call on '
+      + this.addTwoIntsClient.name
+      + ': '
+      + result.sum);
+    this.num = result.num;
+  }
+
   connect() {
     this.ros = new Ros({
       url: 'ws://localhost:9090'
@@ -49,43 +59,79 @@ export class BasicRosFunctinalityComponent implements OnInit {
   }
 
   disconnect() {
-    if(this.connected){
+    if (this.connected) {
       this.ros.close();
     }
   }
 
-  publishTopic(){
-    if(this.connected) {
+  publishTopic() {
+    if (this.connected) {
       let cmdVel = new Topic({
-        ros : this.ros,
-        name : '/cmd_vel',
-        messageType : 'geometry_msgs/Twist' 
+        ros: this.ros,
+        name: '/cmd_vel',
+        messageType: 'geometry_msgs/Twist'
       });
 
       let twist = new Message({
-        linear : {
-          x : 0.1,
-          y : 0.2,
-          z : 0.3
+        linear: {
+          x: 0.1,
+          y: 0.2,
+          z: 0.3
         },
-        angular : {
-          x : -0.1,
-          y : -0.2,
-          z : -0.3
+        angular: {
+          x: -0.1,
+          y: -0.2,
+          z: -0.3
         }
       });
       cmdVel.publish(twist);
     }
   }
 
-  subscribeTopic(){
-    this.listener = new Topic({
-      ros : this.ros,
-      name : '/listener',
-      messageType : 'std_msgs/String'
-    });
+  subscribeTopic() {
+    if(this.connected){
+      this.listener = new Topic({
+        ros: this.ros,
+        name: '/listener',
+        messageType: 'std_msgs/String'
+      });
   
-    this.listener.subscribe(this.subscribeEvent);  
+      this.listener.subscribe(this.subscribeEvent);  
+    }
   }
 
+  callService() {
+    if(this.connected){
+      this.addTwoIntsClient = new Service({
+        ros: this.ros,
+        name: '/add_two_ints',
+        serviceType: 'rospy_tutorials/AddTwoInts'
+      });
+  
+      let request = new ServiceRequest({
+        a: 1,
+        b: 2
+      });
+  
+      this.addTwoIntsClient.callService(request, this.serviceEvent);  
+    }
+  }
+
+  getAndSetParam() {
+    if(this.connected) {
+      this.ros.getParams(function (params) {
+        console.log(params);
+      });
+  
+      var maxVelX = new Param({
+        ros: this.ros,
+        name: 'max_vel_y'
+      });
+  
+      maxVelX.set(0.8);
+      maxVelX.get(function (value) {
+        console.log('MAX VAL: ' + value);
+      });
+    }
+  }
 }
